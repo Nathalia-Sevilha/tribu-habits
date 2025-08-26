4966c6a3f4f784a3dd736643ff824b23f3d00b2d
Nathalia Sevilha <nathaliasevilha@gmail.com>
2025-08-26 16:01:49 +0200

    typo

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
   belongs_to :user
   belongs_to :community
 
-  validades :title, presence: true
+  validates :title, presence: true
   validates :content, presence: true
 end
