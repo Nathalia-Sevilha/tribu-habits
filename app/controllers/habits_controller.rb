123e5e7345278263d72b1307511054a860658c5a
Nathalia Sevilha <nathaliasevilha@gmail.com>
2025-08-26 12:51:55 +0200

    pundit showing index and show for habits

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -1,6 +1,12 @@
 class HabitsController < ApplicationController
-  def index;end
-  def show;end
+  def index
+    @habits = policy_scope(Habit)
+  end
+
+  def show
+    authorize @habit
+  end
+
   def new;end
   def create;end
   def edit;end
--- app/policies/habit_policy.rb
+++ app/policies/habit_policy.rb
@@ -7,8 +7,10 @@ class HabitPolicy < ApplicationPolicy
 
   class Scope < ApplicationPolicy::Scope
     # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+    def resolve
+      scope.all
+      # scope.where(user: user) # If users can only see their habits
+      # scope.where("name LIKE 't%'") # If users can only see habits starting with `t`
+    end
   end
 end
