2b9b4eaffc7fcc87a8e9749ccc6f008119d6fbd6
Atefe Saeedi <atefe.saeedi1994@gmail.com>
2025-08-26 13:54:36 +0200

    color valiables are added

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  root "home#index"
+  root "habits#index"
 
   resources :habits
   resources :communities, only: :show
