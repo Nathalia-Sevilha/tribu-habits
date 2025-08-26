1a9a5586b920c9694d2bcac3c6226c61f61a0426
Atefe Saeedi <atefe.saeedi1994@gmail.com>
2025-08-26 17:32:19 +0200

    comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,34 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :find_post, only: :create
 
-  def create;end
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.post = @post
+    @comment.user = current_user
+    if @comment.save
+      redirect_to post_path(@post), notice: 'comment was successfully created.'
+    else
+      @comments = @post.comments.includes(:user).order(created_at: :asc)
+      flash.now[:alert] = "Could not create the comment."
+      render "posts/show", status: :unprocessable_entity
+    end
+  end
 
-  def destroy;end
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post), notice: "Comment was deleted."
+  end
+
+
+  private
+
+  def find_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
