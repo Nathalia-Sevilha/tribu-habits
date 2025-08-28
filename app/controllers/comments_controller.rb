353ded1a33f681fb92c6f6fc26bc2478f8c19ba2
Atefe Saeedi <atefe.saeedi1994@gmail.com>
2025-08-28 13:31:37 +0200

    create comment added

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,21 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
   before_action :find_post, only: :create
+  before_action :set_context, only: [:new, :create, :destroy]
+
+  skip_after_action :verify_authorized, only: [:new, :create, :destroy]
+
+  def new
+    @comment   = @post.comments.build
+    authorize @comment
+  end
 
   def create
     @comment = Comment.new(comment_params)
     @comment.post = @post
     @comment.user = current_user
     if @comment.save
-      redirect_to post_path(@post), notice: 'comment was successfully created.'
+      redirect_to community_post_path(@community, @post)
     else
       @comments = @post.comments.includes(:user).order(created_at: :asc)
       flash.now[:alert] = "Could not create the comment."
@@ -30,5 +38,10 @@ class CommentsController < ApplicationController
 
   def comment_params
     params.require(:comment).permit(:content)
+  end
+
+   def set_context
+    @community = Community.find(params[:community_id])
+    @post      = @community.posts.find(params[:post_id]) # ensures nesting is consistent
   end
 end
