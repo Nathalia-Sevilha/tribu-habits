d9a62cbe29ab64f1306b8eb61eca10b228f020f2
jessica mota <jessicam.arq.urb@gmail.com>
2025-08-29 12:27:00 +0200

    setup ai

---
+++ app/models/message.rb
@@ -0,0 +1,6 @@
+class Message < ApplicationRecord
+  acts_as_message
+  belongs_to :chat
+  validates :content, presence: true, length: { minimum: 2, maximum: 1000 }, if: -> { role == 'user' }
+  scope :visible, -> { where(role: [:user, :assistant]).where.not(content: ["", nil]) }
+end
