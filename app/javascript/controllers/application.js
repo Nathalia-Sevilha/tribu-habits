12693d1e3137d5b6c3522b81a5d2be6045ea65bb
Nathalia Sevilha <nathaliasevilha@gmail.com>
2025-08-29 17:18:45 +0200

    fixes

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,12 +1,8 @@
 import { Application } from "@hotwired/stimulus"
-// import PhotoUploadController from "./photo_upload_controller"
-
 
 const application = Application.start()
 
-// Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
-// Stimulus.register("photo-upload", PhotoUploadController);
 
 export { application }
