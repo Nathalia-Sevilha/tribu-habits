9cdd8b15411d09b46689b040f3c37ba4a5e74e14
Rayan Walidi-Alaoui <wldrayan21@gmail.com>
2025-11-27 17:57:57 +0100

    habit error message

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -6,4 +6,14 @@ class Habit < ApplicationRecord
   accepts_nested_attributes_for :day_habits, allow_destroy: true
   validates :title, presence: true, uniqueness: { scope: :user }
   validates :streak, numericality: { greater_than_or_equal_to: 0 }
+
+  validate :must_have_days
+
+  private
+
+  def must_have_days
+    if day_ids.blank? || day_ids.reject(&:blank?).empty?
+      errors.add(:base, "Please select at least one day.")
+    end
+  end
 end
